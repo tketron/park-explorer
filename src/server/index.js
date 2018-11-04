a84ccb6444d14c9f65876f1bfd068a0ce9433a0f
Tyler Ketron <tketron@gmail.com>
2018-11-04 15:47:51 -0800

    setup express server to serve static react files

--- src/server/index.js
+++ src/server/index.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 
 app.use(cors());
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, '../../build')));
 
-app.get('/', (req, res) => {
+app.get('/hello', (req, res) => {
   res.send('Hello World');
+});
+
+// Anything that doesn't match the above, send back index.html
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../build/index.html'));
 });
 
 const PORT = process.env.PORT || 5000;
