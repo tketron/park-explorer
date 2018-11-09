efc5e5b84b18d4f289fa080dd1dba966d027d0b9
Tyler Ketron <tketron@gmail.com>
2018-11-08 21:43:20 -0800

    added top navbar and side menu

--- src/client/App/index.js
+++ src/client/App/index.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import AppBar from '@material-ui/core/AppBar';
+import Drawer from '@material-ui/core/Drawer';
+import IconButton from '@material-ui/core/IconButton';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import MenuIcon from '@material-ui/icons/Menu';
+
 import ParkList from '../ParkList';
 import ParkDetails from '../ParkDetails';
 
@@ -8,18 +14,45 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      currentPark: null
+      currentPark: null,
+      open: true
     };
   }
 
   updateCurrentPark = park => {
-    this.setState({ currentPark: park });
+    this.setState({ currentPark: park, open: false });
+  };
+
+  handleDrawerOpen = () => {
+    this.setState({ open: true });
+  };
+
+  handleDrawerClose = () => {
+    this.setState({ open: false });
   };
 
   render() {
     return (
       <div className="App">
-        <ParkList onUpdatePark={this.updateCurrentPark} />
+        <AppBar className={this.state.open}>
+          <IconButton
+            color="inherit"
+            aria-label="Open drawer"
+            onClick={this.handleDrawerOpen}
+            className={this.state.open}
+          >
+            <MenuIcon />
+          </IconButton>
+          PARK EXPLORER
+        </AppBar>
+        <Drawer variant="temporary" open={this.state.open}>
+          <div>
+            <IconButton onClick={this.handleDrawerClose}>
+              <ChevronLeftIcon />
+            </IconButton>
+          </div>
+          <ParkList onUpdatePark={this.updateCurrentPark} />
+        </Drawer>
         {this.state.currentPark && (
           <ParkDetails park={this.state.currentPark} />
         )}
--- src/client/ParkList/index.js
+++ src/client/ParkList/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import axios from 'axios';
 
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+
 import ParkListItem from '../ParkListItem';
 
 class ParkList extends React.Component {
@@ -22,16 +26,21 @@ class ParkList extends React.Component {
     return (
       <div>
         <div>
-          <h1>Parks List</h1>
-          {this.state.parks.map((park, idx) => {
-            return (
-              <ParkListItem
-                key={idx}
-                name={park.name}
-                onClick={() => this.props.onUpdatePark(park)}
-              />
-            );
-          })}
+          <List>
+            <h1>Parks List</h1>
+            {this.state.parks.map((park, idx) => {
+              return (
+                // <ParkListItem
+                //   key={idx}
+                //   name={park.name}
+                //   onClick={() => this.props.onUpdatePark(park)}
+                // />
+                <ListItem button onClick={() => this.props.onUpdatePark(park)}>
+                  <ListItemText primary={park.name} />
+                </ListItem>
+              );
+            })}
+          </List>
         </div>
       </div>
     );
